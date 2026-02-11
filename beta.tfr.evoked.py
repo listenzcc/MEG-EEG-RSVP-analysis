4cb74484d360c1d0946a883ca8f6aca26d7a02bd
listenzcc <chuncheng.zhang@ia.ac.cn>
2026-02-11 11:13:51 +0800

    Adding beta.tft.evoked.py

---
+++ beta.tfr.evoked.py
@@ -0,0 +1,37 @@
+"""
+File: beta.tfr.evoked.py
+Author: Chuncheng Zhang
+Date: 2026-02-10
+Copyright & Email: chuncheng.zhang@ia.ac.cn
+
+Purpose:
+    Compute TFR for evoked.
+
+Functions:
+    1. Requirements and constants
+    2. Function and class
+    3. Play ground
+    4. Pending
+    5. Pending
+"""
+
+
+# %% ---- 2026-02-10 ------------------------
+# Requirements and constants
+from util.easy_imports import *
+
+
+# %% ---- 2026-02-10 ------------------------
+# Function and class
+
+
+# %% ---- 2026-02-10 ------------------------
+# Play ground
+
+
+# %% ---- 2026-02-10 ------------------------
+# Pending
+
+
+# %% ---- 2026-02-10 ------------------------
+# Pending
