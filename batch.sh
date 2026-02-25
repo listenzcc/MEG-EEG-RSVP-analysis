2964ccf1af1f56d8d18ae32c61991ba123ae27b1
listenzcc <chuncheng.zhang@ia.ac.cn>
2026-02-25 11:21:30 +0800

    Archive old results and codes. Add pipeline folder for startup.

--- batch.sh
+++ batch.sh
@@ -8,73 +8,28 @@ echo python env
 which python
 python --version
 
+script=pipeline/1.raw.to.epochs.py
+
 mode=EEG
-
-# script=1.1.plot.evoked.py
-
-# python $script S01 $mode &
-# python $script S02 $mode &
-# python $script S03 $mode &
-# python $script S04 $mode &
-# python $script S05 $mode &
-# python $script S06 $mode &
-# python $script S07 $mode &
-# python $script S08 $mode &
-# python $script S09 $mode &
-# python $script S10 $mode
-
-script=1.1.a.plot.evoked.all.subjects.py
-
-python $script $mode
+python $script S01 $mode &
+python $script S02 $mode &
+python $script S03 $mode &
+python $script S04 $mode &
+python $script S05 $mode &
+python $script S06 $mode &
+python $script S07 $mode &
+python $script S08 $mode &
+python $script S09 $mode &
+python $script S10 $mode
 
 mode=MEG
-
-# script=1.1.plot.evoked.py
-
-# python $script S01 $mode &
-# python $script S02 $mode &
-# python $script S03 $mode &
-# python $script S04 $mode &
-# python $script S05 $mode &
-# python $script S06 $mode &
-# python $script S07 $mode &
-# python $script S08 $mode &
-# python $script S09 $mode &
-# python $script S10 $mode
-
-script=1.1.a.plot.evoked.all.subjects.py
-
-python $script $mode
-
-# script=2.2.detect.erp.py
-
-# python $script S01 $mode &
-# python $script S02 $mode &
-# python $script S03 $mode &
-# python $script S04 $mode &
-# python $script S05 $mode &
-# python $script S06 $mode &
-# python $script S07 $mode &
-# python $script S08 $mode &
-# python $script S09 $mode &
-# python $script S10 $mode
-
-# script=3.concat.subjects.py
-
-# python $script $mode
-
-# script=4.erp.of.roi.channels.py
-
-# python $script $mode
-
-# mode=MEG
-# python $script S01 $mode &
-# python $script S02 $mode &
-# python $script S03 $mode &
-# python $script S04 $mode &
-# python $script S05 $mode &
-# python $script S06 $mode &
-# python $script S07 $mode &
-# python $script S08 $mode &
-# python $script S09 $mode &
-# python $script S10 $mode
+python $script S01 $mode &
+python $script S02 $mode &
+python $script S03 $mode &
+python $script S04 $mode &
+python $script S05 $mode &
+python $script S06 $mode &
+python $script S07 $mode &
+python $script S08 $mode &
+python $script S09 $mode &
+python $script S10 $mode
